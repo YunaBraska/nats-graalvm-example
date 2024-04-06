6d76750f8de68a3a0ee5d69e91c0602d77c4290f
Yuna <io@yuna.berlin>
2024-04-06 10:41:30 +0200

    style: made things more readable

--- README.md
+++ README.md
@@ -10,12 +10,32 @@ configurations and techniques for leveraging NATS in high-performance, low-footp
 ### Native Builds
 
 * Maven build native executable _(see [pom file](pom.xml))_: `./mvnw clean package -Pnative`
-* CLI min _(
-  replace `your-application-classpath`)_: `native-image --initialize-at-run-time=io.nats.client.support.RandomUtils --initialize-at-run-time=java.security.SecureRandom -H:Name=ExampleApp -cp your-application-classpath berlin.yuna.nativeexample.Main`
-* CLI strict _(
-  replace `your-application-classpath`)_: `native-image --no-fallback --no-server --initialize-at-build-time --initialize-at-run-time=io.nats.client.support.RandomUtils --initialize-at-run-time=java.security.SecureRandom -H:Name=ExampleApp -cp your-application-classpath berlin.yuna.nativeexample.Main`
+* CLI minimal _(
+  replace `your-application-classpath`)_:
 
-### GraalVM Parameter Explanation
+```shell
+    native-image \
+    --initialize-at-run-time=io.nats.client.support.RandomUtils \
+    --initialize-at-run-time=java.security.SecureRandom \
+    -H:Name=native-executable \
+    -cp your-application-classpath berlin.yuna.nativeexample.Main
+```
+
+* CLI strict _(
+  replace `your-application-classpath`)_:
+
+```shell
+    native-image \
+    --no-fallback \
+    --no-server \
+    --initialize-at-build-time \
+    --initialize-at-run-time=io.nats.client.support.RandomUtils \
+    --initialize-at-run-time=java.security.SecureRandom \
+    -H:Name=native-executable \
+    -cp your-application-classpath berlin.yuna.nativeexample.Main
+```
+
+### GraalVM parameter explanation
 
 | Parameter                                                     | Description                                                                                                                                                                                                                                                                                                                                                                                                                                     |
 |---------------------------------------------------------------|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
@@ -23,8 +43,12 @@ configurations and techniques for leveraging NATS in high-performance, low-footp
 | `--initialize-at-build-time`                                  | Instructs GraalVM to initialize specified classes at build time rather than at runtime. This can improve startup time but requires careful management of class initialization to avoid errors.                                                                                                                                                                                                                                                  |
 | `--initialize-at-run-time=io.nats.client.support.RandomUtils` | Specifies that the `io.nats.client.support.RandomUtils` class should be initialized at runtime. This is needed because this class likely uses or initializes `Random` or `SecureRandom` instances in static fields. Static initialization at build time would cause these instances to have a fixed seed and not behave as expected at runtime, reducing randomness and potentially affecting the security or functionality of the application. |
 | `--initialize-at-run-time=java.security.SecureRandom`         | Indicates that the `java.security.SecureRandom` class should be initialized at runtime. `SecureRandom` is used for cryptographic operations and relies on entropy sources that are only available at runtime. Initializing it at build time would result in a lack of proper randomness as the entropy sources would not be properly utilized, potentially compromising security.                                                               |
-| `-H:Name=ExampleApp`                                          | Sets the name of the generated executable file.                                                                                                                                                                                                                                                                                                                                                                                                 |
+| `-H:Name=native-executable`                                   | Sets the name of the generated executable file.                                                                                                                                                                                                                                                                                                                                                                                                 |
 | `-cp "<class-path>"`                                          | Specifies the classpath, which includes paths to all classes and libraries that your application depends on. Separate multiple paths with `:` on Unix-like systems or `;` on Windows.                                                                                                                                                                                                                                                           |
 | `<main-class>`                                                | The fully qualified name of your main class. This is the entry point of your application.                                                                                                                                                                                                                                                                                                                                                       |
 
+### Application description
+
+This example uses [jNats](https://github.com/nats-io/nats.java) client to connect to
+the [Nats Server](https://github.com/YunaBraska/nats-server) which is started within this project.
 
--- src/main/java/berlin/yuna/nativeexample/Main.java
+++ src/main/java/berlin/yuna/nativeexample/Main.java
@@ -17,19 +17,25 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
-
+@SuppressWarnings({
+    "java:S106", // Standard outputs should not be used directly to log anything
+    "java:S1192", // String literals should not be duplicated
+    "java:S2259" // Null pointers should not be referenced
+})
 public class Main {
 
     public static void main(String[] args) {
+        // Start the NATS server
         try (final berlin.yuna.natsserver.logic.Nats server = new berlin.yuna.natsserver.logic.Nats().start()) {
 
+            // Connect client to the NATS server
             try (Connection nc = Nats.connect(new Options.Builder().server(server.url()).build())) {
                 // endpoints can be created ahead of time
                 // or created directly by the ServiceEndpoint builder.
                 Endpoint epEcho = Endpoint.builder()
-                        .name("EchoEndpoint")
-                        .subject("echo")
-                        .build();
+                    .name("EchoEndpoint")
+                    .subject("echo")
+                    .build();
 
                 // Sort is going to be grouped. This will affect the actual subject
                 Group sortGroup = new Group("sort");
@@ -38,49 +44,52 @@ public class Main {
                 // - We will reuse an endpoint definition, so we make it ahead of time
                 // - For echo, we could have reused a handler as well, if we wanted to.
                 ServiceEndpoint seEcho1 = ServiceEndpoint.builder()
-                        .endpoint(epEcho)
-                        .handler(msg -> handleEchoMessage(nc, msg, "S1E")) // see below: handleEchoMessage below
-                        .statsDataSupplier(new ExampleStatsDataSupplier()) // see below: ExampleStatsDataSupplier
-                        .build();
+                    .endpoint(epEcho)
+                    .handler(msg -> handleEchoMessage(nc, msg, "S1E")) // see below: handleEchoMessage below
+                    .statsDataSupplier(new ExampleStatsDataSupplier()) // see below: ExampleStatsDataSupplier
+                    .build();
 
                 ServiceEndpoint seEcho2 = ServiceEndpoint.builder()
-                        .endpoint(epEcho)
-                        .handler(msg -> handleEchoMessage(nc, msg, "S2E"))
-                        .build();
+                    .endpoint(epEcho)
+                    .handler(msg -> handleEchoMessage(nc, msg, "S2E"))
+                    .build();
 
                 // you can make the Endpoint directly on the Service Endpoint Builder
                 ServiceEndpoint seSort1A = ServiceEndpoint.builder()
-                        .group(sortGroup)
-                        .endpointName("SortEndpointAscending")
-                        .endpointSubject("ascending")
-                        .handler(msg -> handleSortAscending(nc, msg, "S1A"))
-                        .build();
+                    .group(sortGroup)
+                    .endpointName("SortEndpointAscending")
+                    .endpointSubject("ascending")
+                    .handler(msg -> handleSortAscending(nc, msg))
+                    .build();
 
                 // you can also make an endpoint with a constructor instead of a builder.
                 Endpoint endSortD = new Endpoint("SortEndpointDescending", "descending");
                 ServiceEndpoint seSort1D = ServiceEndpoint.builder()
-                        .group(sortGroup)
-                        .endpoint(endSortD)
-                        .handler(msg -> handlerSortDescending(nc, msg, "S1D"))
-                        .build();
+                    .group(sortGroup)
+                    .endpoint(endSortD)
+                    .handler(msg -> handlerSortDescending(nc, msg))
+                    .build();
 
                 // Create the service from service endpoints.
+                String serviceName1 = "Service1";
+                String serviceName2 = "Service2";
+
                 Service service1 = new ServiceBuilder()
-                        .connection(nc)
-                        .name("Service1")
-                        .description("Service1 Description") // optional
-                        .version("0.0.1")
-                        .addServiceEndpoint(seEcho1)
-                        .addServiceEndpoint(seSort1A)
-                        .addServiceEndpoint(seSort1D)
-                        .build();
+                    .connection(nc)
+                    .name(serviceName1)
+                    .description("Service1 Description") // optional
+                    .version("0.0.1")
+                    .addServiceEndpoint(seEcho1)
+                    .addServiceEndpoint(seSort1A)
+                    .addServiceEndpoint(seSort1D)
+                    .build();
 
                 Service service2 = new ServiceBuilder()
-                        .connection(nc)
-                        .name("Service2")
-                        .version("0.0.1")
-                        .addServiceEndpoint(seEcho2) // another of the echo type
-                        .build();
+                    .connection(nc)
+                    .name(serviceName2)
+                    .version("0.0.1")
+                    .addServiceEndpoint(seEcho2) // another of the echo type
+                    .build();
 
                 System.out.println("\n" + service1);
                 System.out.println("\n" + service2);
@@ -101,19 +110,19 @@ public class Main {
                     String subject = "echo";
                     CompletableFuture<Message> reply = nc.request(subject, request.getBytes());
                     String response = new String(reply.get().getData());
-                    System.out.println(x + ". Called " + subject + " with [" + request + "] Received " + response);
+                    System.out.println(x + ". Called " + infoString(subject, request, response));
                 }
 
                 // sort subjects are formed this way because the endpoints have groups
                 String subject = "sort.ascending";
                 CompletableFuture<Message> reply = nc.request(subject, request.getBytes());
                 String response = new String(reply.get().getData());
-                System.out.println("1. Called " + subject + " with [" + request + "] Received " + response);
+                System.out.println("1. Called " + infoString(subject, request, response));
 
                 subject = "sort.descending";
                 reply = nc.request(subject, request.getBytes());
                 response = new String(reply.get().getData());
-                System.out.println("1. Called " + subject + " with [" + request + "] Received " + response);
+                System.out.println("1. Called " + infoString(subject, request, response));
 
                 // ----------------------------------------------------------------------------------------------------
                 // discovery
@@ -126,11 +135,11 @@ public class Main {
                 List<PingResponse> pingResponses = discovery.ping();
                 printDiscovery("Ping", "[All]", pingResponses);
 
-                pingResponses = discovery.ping("Service1");
-                printDiscovery("Ping", "Service1", pingResponses);
+                pingResponses = discovery.ping(serviceName1);
+                printDiscovery("Ping", serviceName1, pingResponses);
 
-                pingResponses = discovery.ping("Service2");
-                printDiscovery("Ping", "Service2", pingResponses);
+                pingResponses = discovery.ping(serviceName2);
+                printDiscovery("Ping", serviceName2, pingResponses);
 
                 // ----------------------------------------------------------------------------------------------------
                 // info discover variations
@@ -138,11 +147,11 @@ public class Main {
                 List<InfoResponse> infoResponses = discovery.info();
                 printDiscovery("Info", "[All]", infoResponses);
 
-                infoResponses = discovery.info("Service1");
-                printDiscovery("Info", "Service1", infoResponses);
+                infoResponses = discovery.info(serviceName1);
+                printDiscovery("Info", serviceName1, infoResponses);
 
-                infoResponses = discovery.info("Service2");
-                printDiscovery("Info", "Service2", infoResponses);
+                infoResponses = discovery.info(serviceName2);
+                printDiscovery("Info", serviceName2, infoResponses);
 
                 // ----------------------------------------------------------------------------------------------------
                 // stats discover variations
@@ -150,11 +159,11 @@ public class Main {
                 List<StatsResponse> statsResponseList = discovery.stats();
                 printDiscovery("Stats", "[All]", statsResponseList);
 
-                statsResponseList = discovery.stats("Service1");
-                printDiscovery("Stats", "Service1", statsResponseList); // will show echo without data decoder
+                statsResponseList = discovery.stats(serviceName1);
+                printDiscovery("Stats", serviceName1, statsResponseList); // will show echo without data decoder
 
-                statsResponseList = discovery.stats("Service2");
-                printDiscovery("Stats", "Service2", statsResponseList);
+                statsResponseList = discovery.stats(serviceName2);
+                printDiscovery("Stats", serviceName2, statsResponseList);
 
                 // ----------------------------------------------------------------------------------------------------
                 // stop the service
@@ -165,20 +174,26 @@ public class Main {
                 // stopping the service will complete the futures received when starting the service
                 System.out.println("\nService 1 stopped ? " + serviceStoppedFuture1.get(1, TimeUnit.SECONDS));
                 System.out.println("Service 2 stopped ? " + serviceStoppedFuture2.get(2, TimeUnit.SECONDS));
-            } catch (Exception e) {
+            } catch (final InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            } catch (final Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private static JsonValue replyBody(String label, byte[] data, String handlerId) {
-        return JsonValueUtils.mapBuilder()
-                .put(label, new String(data))
-                .put("hid", handlerId)
-                .toJsonValue();
+    private static String infoString(final String subject, final String request, final String response) {
+        return subject + " with [" + request + "] Received " + response;
     }
 
-    private static void handlerSortDescending(Connection nc, ServiceMessage smsg, String handlerId) {
+    private static JsonValue replyBody(final String label, final byte[] data, final String handlerId) {
+        return JsonValueUtils.mapBuilder()
+            .put(label, new String(data))
+            .put("hid", handlerId)
+            .toJsonValue();
+    }
+
+    private static void handlerSortDescending(final Connection nc, final ServiceMessage smsg) {
         byte[] data = smsg.getData();
         Arrays.sort(data);
         int len = data.length;
@@ -186,21 +201,21 @@ public class Main {
         for (int x = 0; x < len; x++) {
             descending[x] = data[len - x - 1];
         }
-        smsg.respond(nc, replyBody("sort_descending", descending, handlerId));
+        smsg.respond(nc, replyBody("sort_descending", descending, "S1D"));
     }
 
-    private static void handleSortAscending(Connection nc, ServiceMessage smsg, String handlerId) {
+    private static void handleSortAscending(final Connection nc, final ServiceMessage smsg) {
         byte[] ascending = smsg.getData();
         Arrays.sort(ascending);
-        smsg.respond(nc, replyBody("sort_ascending", ascending, handlerId));
+        smsg.respond(nc, replyBody("sort_ascending", ascending, "S1A"));
     }
 
-    private static void handleEchoMessage(Connection nc, ServiceMessage smsg, String handlerId) {
+    private static void handleEchoMessage(final Connection nc, final ServiceMessage smsg, final String handlerId) {
         smsg.respond(nc, replyBody("echo", smsg.getData(), handlerId));
     }
 
     @SuppressWarnings("rawtypes")
-    private static void printDiscovery(String action, String label, List objects) {
+    private static void printDiscovery(final String action, final String label, final List objects) {
         System.out.println("\n" + action + " " + label);
         for (Object o : objects) {
             System.out.println("  " + o);
@@ -208,10 +223,10 @@ public class Main {
     }
 
     static class ExampleStatsData implements JsonSerializable {
-        public String sData;
-        public int iData;
+        private final String sData;
+        private final int iData;
 
-        public ExampleStatsData(String sData, int iData) {
+        public ExampleStatsData(final String sData, final int iData) {
             this.sData = sData;
             this.iData = iData;
         }
